31e7ea3d11316dedfa954fc9a1f66a38802b59c9
Alexandre de Oliveira Garcia <gr.alexandre.oliveira@gmail.com>
2021-03-20 14:11:40 -0300

    Commit incial do projeto

---
+++ src/test/java/com/ct/controletarefas/api/ControleTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.controletarefas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
